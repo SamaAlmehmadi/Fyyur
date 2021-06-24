a5ea7abfb1c1c1edfe8074f99b83e358292731bb
SamaAlmehmadi <85417706+SamaAlmehmadi@users.noreply.github.com>
2021-06-24 17:34:56 +0300

    Update config.py
    add " WTF_CSRF_ENABLED = False ,SECRET_KEY = os.urandom(32)"

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+WTF_CSRF_ENABLED = False
 # Enable debug mode.
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Connect to the database
-
+SECRET_KEY = os.urandom(32)
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI =  'postgresql://postgres:1234@localhost:5432/fyyur_01'
+SQLALCHEMY_DATABASE_URI =  'postgresql://postgres:1234@localhost:5432/fyyur_02'
